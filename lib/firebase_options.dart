b387fc8353b28f78a7a2b9e0f69437bccecffee0
guido gutierrez <guidogutierrez.25@gmail.com>
2024-12-11 15:48:19 -0500

    init

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,7 +17,10 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return web;
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -25,9 +28,15 @@ class DefaultFirebaseOptions {
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
-        return macos;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.windows:
-        return windows;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.linux:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for linux - '
@@ -40,19 +49,9 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyCfd5aKIiHzViZypd8iXSOt4SeLPz3NFtM',
-    appId: '1:526668543292:web:6b6506baed89bc5b7f1a39',
-    messagingSenderId: '526668543292',
-    projectId: 'smart-menu-9387c',
-    authDomain: 'smart-menu-9387c.firebaseapp.com',
-    storageBucket: 'smart-menu-9387c.firebasestorage.app',
-    measurementId: 'G-HQETHHGGZ1',
-  );
-
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyA2kVLZPDzH1awbo5ilhaCM08OH5ZfihMM',
-    appId: '1:526668543292:android:2da312bde188cc1f7f1a39',
+    appId: '1:526668543292:android:90db059e0045ce487f1a39',
     messagingSenderId: '526668543292',
     projectId: 'smart-menu-9387c',
     storageBucket: 'smart-menu-9387c.firebasestorage.app',
@@ -60,29 +59,10 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyDMYi-ZiVv5Plh0QBscv9EcGp7opSgJen0',
-    appId: '1:526668543292:ios:e8ee8a54c5a32a657f1a39',
+    appId: '1:526668543292:ios:c3450162f1a0b4e67f1a39',
     messagingSenderId: '526668543292',
     projectId: 'smart-menu-9387c',
     storageBucket: 'smart-menu-9387c.firebasestorage.app',
-    iosBundleId: 'com.example.smartmenu',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDMYi-ZiVv5Plh0QBscv9EcGp7opSgJen0',
-    appId: '1:526668543292:ios:e8ee8a54c5a32a657f1a39',
-    messagingSenderId: '526668543292',
-    projectId: 'smart-menu-9387c',
-    storageBucket: 'smart-menu-9387c.firebasestorage.app',
-    iosBundleId: 'com.example.smartmenu',
-  );
-
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyCfd5aKIiHzViZypd8iXSOt4SeLPz3NFtM',
-    appId: '1:526668543292:web:5171fcb710fe74477f1a39',
-    messagingSenderId: '526668543292',
-    projectId: 'smart-menu-9387c',
-    authDomain: 'smart-menu-9387c.firebaseapp.com',
-    storageBucket: 'smart-menu-9387c.firebasestorage.app',
-    measurementId: 'G-T73QP3LS3D',
+    iosBundleId: 'cl.erpsystems.smartmenu',
   );
 }
